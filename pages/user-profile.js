1857c087707a5e39582ab1c856e267f509550a1b
رضائی - محمد <rezaee-moha@tehran.iri>
2022-07-19 05:54:54 +0430

    user Profile

---
+++ pages/user-profile.js
@@ -0,0 +1,16 @@
+function UserProfile(props){
+    return <h1>{props.username}</h1>
+}
+
+export default UserProfile;
+
+
+export async function getServerSideProps(context){
+    const {params , req , res} = context;
+    return{
+        props : {
+            username : 'Mohammad'
+        }
+    };
+
+}
